52d3d8623e9577ffdac0981e809244b53cbbc2f1
Andrii Dobrus <58091875+MuffinKing-jpeg@users.noreply.github.com>
2023-06-21 22:38:50 +0300

    Small style improvements

--- src/app/modules/random/random.module.ts
+++ src/app/modules/random/random.module.ts
@@ -3,18 +3,31 @@ import {CommonModule} from '@angular/common';
 import {RandomComponent} from './random.component';
 import {RouterModule, Routes} from '@angular/router';
 import {NamesComponent} from './names/names.component';
+import {UserComponent} from './user/user.component';
 
 const routes: Routes = [
   {
     path: '',
     component: RandomComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'user'
+      },
+      {
+        path: 'user',
+        component: UserComponent
+      }
+    ]
   }
 ]
 
 @NgModule({
   declarations: [
     RandomComponent,
-    NamesComponent
+    NamesComponent,
+    UserComponent
   ],
   imports: [
     CommonModule,
